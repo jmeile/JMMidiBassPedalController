0347fd8e7b31c2cb80de52790a6708f1f3ed6a16
Josef Meile <jmeile@hotmail.com>
2020-02-29 23:20:04 +0100

    Schema validate was added. Now the input and output ports will be correctly validated and opened

--- src/MidiConnector.py
+++ src/MidiConnector.py
@@ -19,6 +19,7 @@ from autologging import logged
 import sys
 import fnmatch
 from rtmidi import MidiIn, MidiOut
+import xmlschema
 #from MidiProcessor import MidiProcessor
 
 #By default, file logging is enabled
@@ -52,15 +53,22 @@ class MidiConnector:
   Opens the MIDI ports and process the incomming connections
   """
   
-  def __init__(self, args):
-    """Initializes the MidiConnector class"""
+  def __init__(self, args, xsd_schema = 'conf/MidiBassPedalController.xsd'):
+    """
+    Initializes the MidiConnector class
+    Parameters:
+    * args: command-line arguments
+    * xsd_schema: path to the xsd schema
+    """
     self._args = args
+    self._xsd_schema = xsd_schema
     self._midi_in = None
     self._midi_out = None
     self._in_ports = []
     self._in_port = 0
     self._out_ports = []
     self._out_port = 0
+    self._xml_dict = {}
 
   def start(self):
     """
@@ -84,7 +92,11 @@ class MidiConnector:
     if not exit:
       if self._args.list:
         self._list_ports()
-      else:   
+      else:
+        self._parse_xml_config()
+        self._parse_ports()
+        self._open_ports()
+        self._close_ports()
         """
         self._open_ports()
         MidiProcessor(
@@ -99,13 +111,37 @@ class MidiConnector:
           #ignore_timing = False,
           #ignore_active_sense = False,
         ).read_midi()
-        self._close_ports()
         """
-        #Ignore this until the implementation is finished
-        pass
-
     self._free_midi()
-  
+
+  def _parse_xml_config(self):
+    """
+    Parses the specified xml configuration file
+    """
+    exit = False
+    try:
+      xsd_schema = xmlschema.XMLSchema11(self._xsd_schema)
+    except:
+      exit = True
+      error = traceback.format_exc()
+      self.__log.info("Error while parsing xsd file:\n" + self._xsd_schema + \
+                      "\n\n" + error)
+      
+    if not exit:
+      try:
+        xml_dict = xsd_schema.to_dict(self._args.config)
+      except:
+        exit = True
+        error = traceback.format_exc()
+        self.__log.info("Error while parsing xml file:\n" + \
+                        self._args.config + "\n\n" + error)
+    
+    if exit:
+      self._free_midi()
+      sys.exit()
+      
+    self._xml_dict = xml_dict
+
   def _open_port(self, midi_interface, midi_port):
     """
     Opens the specified MIDI port for the entered midi_interface
@@ -152,10 +188,10 @@ class MidiConnector:
     Gets the specified port from command line
     Parameters:
     * port_list: List of available MIDI ports
-    * arg_name: name of the argument to get. It can be: in_port or out_port
+    * arg_name: name of the argument to get. It can be: InPort or OutPort
     """
     num_ports = len(port_list)
-    port_value = getattr(self._args, arg_name)
+    port_value = self._xml_dict.get('@'+arg_name, num_ports)
     if (type(port_value) == str) and port_value.isdigit():
       port_value = int(port_value)
     elif type(port_value) == str:
@@ -173,8 +209,10 @@ class MidiConnector:
         self.__log.info("The " + arg_name + ": " + port_value + " wasn't found.")
         self._free_midi()
         sys.exit()
-      port_value = port_index
+      port_value = port_index + 1
     
+    #Internally, port numbers start from 0 because they are in an array
+    port_value -= 1
     if port_value >= num_ports:
       self.__log.info("Invalid port number was supplied")
       self._free_midi()
@@ -186,8 +224,8 @@ class MidiConnector:
     """
     Gets the passed ports to the command line
     """
-    self._in_port = self._parse_port(self._in_ports, 'in_port')
-    self._out_port = self._parse_port(self._out_ports, 'out_port')
+    self._in_port = self._parse_port(self._in_ports, 'InPort')
+    self._out_port = self._parse_port(self._out_ports, 'OutPort')
   
   def _open_midi(self):
     """Starts MIDI without opening a port"""
@@ -235,44 +273,6 @@ class MidiConnector:
       ports[port_index] = port
       port_index += 1
     return ports
-
-  def _read_port(self, port_list, msg):
-    """
-    Asks the user for a MIDI port to use.
-    Parameters:
-    * port_list: ports from which the user has to choose from
-    * msg: message to show to the user.
-    """
-    num_ports = len(port_list)
-    ports_str = self._get_formatted_port_list(port_list)
-    port = -1
-    while port == -1:
-      self.__log.info(msg)
-      self.__log.info(ports_str)
-      try:
-        port = int(input("Type the port you want to use [0-" + \
-             str(num_ports - 1) + "] or CTRL+C to abort: "))
-      except KeyboardInterrupt:
-        self.__log.info("The operation was aborted")
-        self._free_midi()
-        sys.exit()
-      except:
-        self.__log.info("\nAn unexpected error occured")
-        error = traceback.format_exc()
-        self.__log.info(error + "\n")
-        port = -1
-      if port >= num_ports:
-        port = -1
-        self.__log.info("\nInvalid port number was given\n")
-        
-    return port
-    
-  def _read_ports(self):
-    self._in_port = self._read_port(self._in_ports, "Available MIDI IN "
-      "ports:")
-    print("")
-    self._out_port = self._read_port(self._out_ports, "Available MIDI OUT "
-      "ports:")
 
   def _get_all_ports(self):
     """
